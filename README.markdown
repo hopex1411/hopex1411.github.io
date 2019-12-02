5b773657a5357da4e8834d61ef0129056af624ab
Anton ulrik <42369351+hopex1411@users.noreply.github.com>
2019-12-02 18:05:28 +0100

    add all

---
+++ README.markdown
@@ -0,0 +1,9 @@
+# Simple Advent calendar with message
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/anton-ulrik/pen/pooNxrL](https://codepen.io/anton-ulrik/pen/pooNxrL).
+
+ Just a quick and dirty responsive advent calendar with jQuery that displays a message one word per day. On the 25th the full message is revealed. Works on mobile devices. Uncomment "day=25;" in the js to see the end.
+
+I put this together real quick so there are a couple of negatives:
+1) Words can't be too long due to the fixed dimensions of the boxes.
+2) Door animation not correct in IE due to its handling of transform origin, if I'm right?
+3) You can see the message in the js source... but then again you can also open real advent calendar windows early too and you wouldn't do that either, right? :p
---
+++ script.js
@@ -0,0 +1,82 @@
+$( document ).ready(function() {
+
+  var words = ["img/baglåge1.png", "img/baglåge2.png", "img/baglåge3.png", "img/baglåge4.png", "img/baglåge5.jpg", "img/baglåge6.png", "img/baglåge7.jpg", "img/baglåge8.png", "img/baglåge9.jpg", "img/baglåge10.png", "img/baglåge11.png", "img/baglåge12.png", "img/baglåge13.jpeg", "img/baglåge14.png", "img/baglåge15.jpg", "img/baglåge16.jpg", "img/baglåge17.jpeg", "img/baglåge18.png", "img/baglåge19.png", "img/baglåge20.jpg", "img/baglåge21.png", "img/baglåge22.png", "img/baglåge23.png", "img/baglåge24.png"];
+
+  var message = "";
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var scrolled = false;
+  var timeDelay = 200;
+
+  // function to reveal message
+  var cardReveal = function() {
+    $("#message").text(message).show();
+  }  
+
+  //day=25; // uncomment to skip to 25
+
+  // Only work in December
+  if(month === 12) {
+    // Loop through each calendar window
+    $("li").each( function( index ) {
+      var adventwindow = index + 1;
+      var item = $(this);
+
+      // Open past windows
+      if( day !== adventwindow && adventwindow < day ) {
+        window.setTimeout(function(){
+          item.children(".door").addClass("open");
+        }, timeDelay);
+      }
+
+      // timeout offset for past window opening animation
+      timeDelay += 100;
+
+      // Add words so far to message variable
+      if( adventwindow <= day ) {
+        var word = words[index];
+        $(this).append('<div class="revealed">' + `<img class="baglaage${index}" src="">` + '</div>');
+        message = message + " " + word;
+        document.querySelector('.baglaage'+index).setAttribute('src', word);
+      }
+
+      // Add jiggle animation to current day window
+      if(adventwindow === day) {
+        $(this).addClass("current");
+        $(this).addClass("jiggle");
+      }
+
+      // On clicking a window, toggle it open/closed and
+      // handle other things such as removing jiggle and 25th
+      $(this).on("click", function() {
+        if(adventwindow <= day) { 
+          $(this).children(".door").toggleClass("open");
+        }
+
+        $(this).removeClass("jiggle");
+
+        // If 24th, can show the message
+        if(day >= 24 && adventwindow === 24) {
+          messageReveal();
+
+          // Animate scroll to message if not already done
+          if(!scrolled) {
+            $('html, body').animate({
+              scrollTop: $("#message").offset().top
+            }, 2000);
+            scrolled = true;
+          }
+        }
+      });
+
+    });
+
+    // If beyond 24, show message
+    if(day >= 25){
+      messageReveal();
+    }
+
+  }
+
+});
